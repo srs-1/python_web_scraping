a26534e6a62b60dee00390277f9c95f4fd91c247
srs-1 <swethaaa.ramesh@gmail.com>
2024-07-21 17:33:33 +0530

    Understand how to use beautifulsoup on website data

---
+++ extracting from a website/webscraper-2.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+
+soup = BeautifulSoup(html_text, 'lxml')
+
+jobs = soup.find_all('li',class_ = 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    job_pub_date = job.find('span', class_ = 'sim-posted').span.text
+    if 'few' in job_pub_date:
+        company = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+        print(f'''
+            Company Name : {company},Skills Required : {skills}
+        ''')
